4a30e76cb959222b0285723e39658ec90378bb2f
Anne Burmester <anne.burmester@sogeti.com>
2020-10-29 12:41:32 +0100

    inserted check for existence of g in the string of GHappy

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,38 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class GHappyTest {
+
+    @Test
+    public void unhappyonly(){
+        boolean result = new GHappy().gHappy("xxgxx");
+        Assertions.assertEquals(false,result);
+    }
+
+    @Test
+    public void happyonly(){
+        boolean result = new GHappy().gHappy("xxggxx");
+        Assertions.assertEquals(true, result);
+    }
+
+    @Test
+    public void mixedfirst(){
+        boolean result = new GHappy().gHappy("xxgyyggxx");
+        Assertions.assertEquals(false, result);
+    }
+
+    @Test
+    public void mixedlast(){
+        boolean result = new GHappy().gHappy("xxggyygxx");
+        Assertions.assertEquals(false, result);
+    }
+
+    @Test
+    public void none() {
+        boolean result = new GHappy().gHappy("xxyyxxxyy");
+        Assertions.assertEquals(false, result);
+
+    }
 }
